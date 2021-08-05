3421c4c16cff5d9dcb6d6e7e1578ca65dac58700
marcoxDev <82086004+marcoxDev@users.noreply.github.com>
2021-08-06 00:11:47 +0200

    Add files via upload

---
+++ InstagramTool.py
@@ -0,0 +1,28 @@
+import instaloader as insta
+
+username = "marco_cerby"
+server = insta.Instaloader()
+profile = insta.Profile.from_username(server.context, username)
+
+print(f"Name: {profile.full_name}")
+print(f"Bio: {profile.biography}")
+print(f"Followers: {profile.followers}")
+print(f"Following: {profile.followees}")
+print(f"Posts: {profile.mediacount}")
+print(f"Is private: {profile.is_private}")
+print(f"Is buisness: {profile.is_business_account}")
+
+if profile.is_private == False:
+    posts = profile.get_posts()
+    for post in posts:
+        print(f"\n\nDate: {post.date}")
+        print(f"Likes: {post.likes}")
+        print(f"Description: {post.caption}")
+        print(f"Comments: {post.comments}")
+        print(f"Hashtags: {post.caption_hashtags}")
+        print(f"Location: {post.location}")
+        print(f"Image Url: {post.url}")
+        input()
+else:
+    print("\nThis account is private. No posts found")
+    input()
